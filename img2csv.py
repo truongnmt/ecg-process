3969b8728d21c8c1681cfe08bd0de0473de48e56
TruongNM <manhtruonghedspi@gmail.com>
2018-04-28 22:50:10 +0700

    Fix img to csv bug

--- img2csv.py
+++ img2csv.py
@@ -11,7 +11,6 @@ filename = os.path.splitext(os.path.basename(PATH))[0]
 img = cv2.imread(PATH)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
 indices = np.where(img == [68]) # where pixel is pink (pink -> gray = 68)
 coordinates = np.array(list(zip(indices[1], indices[0])))  # -> coordinates[x, y]
 coordinates = coordinates[coordinates[:,0].argsort()] # sort and calculate mean value of identical items
@@ -21,5 +20,5 @@ for k, v in coordinates:
 coordinates = sorted([(k, sum(v) / len(v)) for k, v in values_by_key.items()])
 
 with open('img2csv/%s-converted.csv' % filename, 'w+') as f:
-    for index, coordinate in enumerate(coordinates):        
-        f.write('{0},{1}\n'.format((coordinate[0]-56)*0.004, round((122 - coordinate[1])/70, 3)))
+    for index, coordinate in enumerate(coordinates):
+        f.write('{0},{1}\n'.format((coordinate[0]-40)*0.004, round((139 - coordinate[1])/41, 3)))
